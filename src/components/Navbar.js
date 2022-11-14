7477f271df5505ed2bea6ca968f4fee94000ee5a
Jones <jonesma@ntnu.no>
2022-11-14 17:32:12 +0100

    much stuff

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import reactLogo from "../images/react-icon.png"
+
+export default function Navbar() {
+    return (
+        <nav className={"header-bar"}>
+                <img className="react-logo" src={reactLogo}  alt={"a react logo"}/>
+                <h3 data-testid="reactfacts">ReactFacts</h3>
+                <h4>React course - project 1</h4>
+        </nav>
+    )
+}
---
+++ src/components/test/NavBar.test.js
@@ -0,0 +1,11 @@
+import Navbar from "../Navbar";
+import {screen, cleanup, render} from "@testing-library/react"
+import '@testing-library/jest-dom'
+
+
+test("navbar displays correct", () => {
+    render(<Navbar />)
+    const navbarFacts = screen.getByTestId("reactfacts");
+    expect(navbarFacts).toBeInTheDocument();
+    expect(navbarFacts).toHaveTextContent("ReactFacts")
+});
