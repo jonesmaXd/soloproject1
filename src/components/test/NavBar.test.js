b7b8325677fe766c4926ed6a40ef618c2abe08b1
Jones <jonesma@ntnu.no>
2022-11-14 18:43:36 +0100

    Test pipeline with failing test

--- src/components/test/NavBar.test.js
+++ src/components/test/NavBar.test.js
@@ -7,5 +7,5 @@ test("navbar displays correct", () => {
     render(<Navbar />)
     const navbarFacts = screen.getByTestId("reactfacts");
     expect(navbarFacts).toBeInTheDocument();
-    expect(navbarFacts).toHaveTextContent("ReactFacts")
+    expect(navbarFacts).toHaveTextContent("")
 });
